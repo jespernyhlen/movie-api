3c409de3d4d00cfe051cb9edbcfe2501badc8f1a
Jesper Nyhlen <jeppe_nyhlen@hotmail.com>
2019-10-31 23:26:51 +0100

    Removed import from removed file

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ mongoose.connect(
     `mongodb+srv://${HOST}:${PASSWORD}@rest-vdyua.mongodb.net/test?retryWrites=true&w=majority`
 );
 
-const Movie = require('./movieModel');
-
 // let id = 14323;
 
 app.use(bodyParser.json());
